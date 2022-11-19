fd58d288bd4f440dd9054a3c4a59f8593d6f0de9
Berry Liu <bl3056@nyu.edu>
2022-11-18 21:21:24 -0500

    Set up Flask Skeleton and SQL connection

---
+++ app.py
@@ -0,0 +1,32 @@
+#Import Flask Library
+from flask import Flask, render_template, request, session, url_for, redirect
+import pymysql.cursors
+
+#Initialize the app from Flask
+app = Flask(__name__)
+
+#Configure MySQL
+conn = pymysql.connect(host='localhost',
+                       port=3306,          
+                       user='root',
+                       password='',
+                       db='FlightReservation',
+                       charset='utf8',
+                       cursorclass=pymysql.cursors.DictCursor)
+
+
+
+
+#Define a route to hello function
+@app.route('/')
+def hello():
+	return render_template('index.html')
+
+
+
+app.secret_key = 'some key that you will never guess'
+#Run the app on localhost port 5000
+#debug = True -> you don't have to restart flask
+#for changes to go through, TURN OFF FOR PRODUCTION
+if __name__ == "__main__":
+	app.run('127.0.0.1', 5000, debug = True)
