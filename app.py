5bf8eee683e9549f05fffd970890547fa3208c59
Berry Liu <bl3056@nyu.edu>
2022-11-20 18:37:04 -0500

    Added login/logout templates and Flask endpoints

--- app.py
+++ app.py
@@ -14,15 +14,55 @@ conn = pymysql.connect(host='localhost',
                        charset='utf8',
                        cursorclass=pymysql.cursors.DictCursor)
 
-
-
+cursor = conn.cursor()
 
 #Define a route to hello function
 @app.route('/')
 def home():
-	return render_template('index.html')
+    if not session.get('username'):
+        return render_template('index.html', username=None, name=None)
+    else:
+        # check if user is customer or admin
+        # render appropriate page
 
+        return render_template('index.html', username=session.get('username'), name=session.get('name'))
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        if session.get('username', None): # check if the user is already logged in
+            return redirect(url_for('home'))
+        else:
+            return render_template('login.html')
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        # check if user is a customer
+        query = 'SELECT name FROM customers WHERE email = %s and password = %s'
+        cursor.execute(query, (username, password))
+        data = cursor.fetchone()
+        error = None
+        if(data): # if user is a customer
+            session['username'] = username
+            session['name'] = data['name'].split()[0]
+            return redirect(url_for('home'))
+        else:
+            # check if user is an airline staff
+            query = 'SELECT first_name FROM airlinestaff WHERE username = %s and password = %s'
+            cursor.execute(query, (username, password))
+            data = cursor.fetchone()
+            if (data):
+                session['username'] = username
+                session['name'] = data['first_name']
+                return redirect(url_for('home'))
+            else: # user is not a customer or airline staff
+                error = 'Invalid username or password. Please try again!'
+                return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('username')
+    return redirect('/')
 
 app.secret_key = 'some key that you will never guess'
 #Run the app on localhost port 5000
