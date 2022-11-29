f3535a90d0d578140e36fca1dbb6032c70634fb4
Quan Phung <quanh.phung1902@gmail.com>
2022-11-28 23:34:29 -0500

    Added staff_view queries to sql_helper.py

--- sql_helper.py
+++ sql_helper.py
@@ -8,7 +8,7 @@ dotenv.load_dotenv()
 #                        port=3306,          
 #                        user='root',
 #                        password='',
-#                        db='FlightReservation',
+#                        db='flight_reservation_prj',
 #                        charset='utf8',
 #                        cursorclass=pymysql.cursors.DictCursor)
 
@@ -87,3 +87,62 @@ def check_register_airlinestaff(data):
     except pymysql.err.IntegrityError as e:
         print('Error: ', e)
         return False
+
+def view_all_flights_staff(data):
+    airline = data.get('airline')
+    departure_time = data.get('departure_time')
+    departure_airport = data.get('departure_airport')
+    arrival_airport = data.get('arrival_airport')
+    query = 'SELECT * FROM flights, airplanes \
+WHERE airplanes.airline = %s \
+AND flights.departure_time > %s AND flights.departure_time < %s \
+AND flights.departure_airport = %s AND flights.arrival_airport = %s'
+    cursor.execute(query, (airline, departure_time, departure_time, departure_airport, arrival_airport))
+    data = cursor.fetchone() # fetchall
+    return data # What will data be?
+
+def view_all_customers_staff(data):
+    flight_num = data.get('flight_num')
+    query = 'SELECT name FROM customers INNER JOIN tickets ON customers.email = tickets.customer_email \
+WHERE tickets.flight_num = %s;'
+    cursor.execute(query, (flight_num))
+    data = cursor.fetchone()
+    return data
+
+def view_ratings_comments(data): # How to fetch data from 2 queires
+    flight_num = data.get('flight_num')
+    query = "SELECT avg(rating) AS 'Average rating' FROM reviews \
+WHERE flight_num = %s;"
+    query = "SELECT rating, comment FROM reviews \
+WHERE flight_num = %s;"
+    cursor.execute(query, (flight_num))
+    data = cursor.fetchone() # Gets all possible result
+    return data
+
+def view_freq_customer(data):
+    query = "SELECT email FROM tickets \
+WHERE COUNT(email) = \
+(SELECT MAX(COUNT(email)) FROM tickets)"
+    cursor.execute(query)
+    data = cursor.fetchone()
+    return data
+
+def view_report(data):
+    departure_time = data.get('departure_time')   
+    query = "Select count(id) AS total_tickets_sold FROM Tickets WHERE id = \
+(SELECT tickets.id FROM tickets JOIN fLights \
+ON tickets.flight_num = flights.flight_num \
+WHERE flights.departure_time > %s AND flights.departure_time < %s)"
+    cursor.execute(query, (departure_time, departure_time))
+    data = cursor.fetchone()
+    return data
+
+def view_revenue(data):
+    departure_time = data.get('departure_time')
+    query = "Select sum(sold_price) AS total_revenue FROM Tickets WHERE id = \
+(SELECT tickets.id FROM tickets JOIN flights \
+ON tickets.flight_num = flights.flight_num \
+WHERE flights.departure_time > %s AND flights.departure_time < %s)"
+    cursor.execute(query, (departure_time, departure_time))
+    data = cursor.fetchone()
+    return data
